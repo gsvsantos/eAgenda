04b2eb50a61408646f85c01b9cb44c1820c4087d
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-16 19:12:47 -0300

    Add project files.

---
+++ eAgenda.WebApp/Program.cs
@@ -0,0 +1,19 @@
+namespace eAgenda.WebApp
+{
+#pragma warning disable RCS1102 // Make class static
+    public class Program
+#pragma warning restore RCS1102 // Make class static
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
+            app.Run();
+        }
+    }
+}
---
+++ eAgenda.WebApp/wwwroot/js/script.js
@@ -0,0 +1,30 @@
+﻿const hamburguer = document.querySelector('.toggle-btn');
+const toggler = document.querySelector('.toggle-btn-icon');
+var minWidth = window.matchMedia("(min-width: 1200px)")
+console.log(minWidth);
+
+function sidebarShow() {
+    document.querySelector('.sidebar').classList.toggle('expand');
+    toggler.classList.toggle('bi-chevron-double-left');
+    toggler.classList.toggle('bi-chevron-double-right');
+}
+
+function sidebarHide() {
+    document.querySelector('.sidebar').classList.remove('expand');
+}
+
+if (!minWidth.matches) {
+    sidebarHide();
+}
+else {
+    hamburguer.addEventListener('click', sidebarShow);
+}
+
+minWidth.onchange = (e) => {
+    if (!e.matches) {
+        sidebarHide();
+    }
+    else {
+        sidebarShow()
+    }
+}
