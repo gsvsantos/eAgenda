1d4b05a1e3d26b1aa1735ab855c7c69f013a5d7e
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-19 23:54:33 -0300

    feat: Implementa ItemTarefa.cs em Dominio/ModuloTarefa.

---
+++ eAgenda.Dominio/ModuloTarefa/ItemTarefa.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace eAgenda.Dominio.ModuloTarefa;
+
+public class ItemTarefa
+{
+    public Guid Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public StatusItemTarefa Status { get; set; }
+
+    [ExcludeFromCodeCoverage]
+    public ItemTarefa() { }
+    public ItemTarefa(string titulo) : this()
+    {
+        Id = Guid.NewGuid();
+        Titulo = titulo;
+        Status = StatusItemTarefa.EmAndamento;
+    }
+
+
+    public void MarcarEmAndamento()
+    {
+        Status = StatusItemTarefa.EmAndamento;
+    }
+
+    public void Concluir()
+    {
+        Status = StatusItemTarefa.Concluido;
+    }
+
+    public void Reabrir()
+    {
+        if (Status == StatusItemTarefa.Concluido || Status == StatusItemTarefa.Cancelado)
+            Status = StatusItemTarefa.EmAndamento;
+    }
+
+    public void Cancelar()
+    {
+        Status = StatusItemTarefa.Cancelado;
+    }
+}
+
+public enum StatusItemTarefa
+{
+    [Display(Name = "Em Andamento")]
+    EmAndamento = 0,
+    [Display(Name = "Concluído")]
+    Concluido = 1,
+    [Display(Name = "Cancelado")]
+    Cancelado = 2,
+}
