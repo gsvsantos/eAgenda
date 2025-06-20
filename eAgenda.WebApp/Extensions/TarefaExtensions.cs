323f8077826f8be349b753afbd2bb2af0335b9b4
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-19 23:55:55 -0300

    feat: Implementa TarefaExtension.cs em WebApp/Extensions.

---
+++ eAgenda.WebApp/Extensions/TarefaExtensions.cs
@@ -0,0 +1,29 @@
+﻿using eAgenda.Dominio.ModuloTarefa;
+using eAgenda.WebApp.Models;
+
+namespace eAgenda.WebApp.Extensions;
+
+public static class TarefaExtensions
+{
+    public static Tarefa ParaEntidade(this FormularioTarefaViewModel formularioVM)
+    {
+        return new(
+            formularioVM.Titulo,
+            formularioVM.Descricao ?? string.Empty,
+            formularioVM.Prioridade ?? NivelPrioridade.Baixa,
+            formularioVM.DataCriacao);
+    }
+    public static DetalhesTarefaViewModel ParaDetalhesVM(this Tarefa tarefa)
+    {
+        return new(
+            tarefa.Id,
+            tarefa.Titulo,
+            tarefa.Descricao,
+            tarefa.Prioridade,
+            tarefa.DataCriacao,
+            tarefa.DataConclusao,
+            tarefa.Status,
+            tarefa.PercentualConcluido,
+            tarefa.Itens);
+    }
+}
