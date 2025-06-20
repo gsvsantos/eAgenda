07c0b5bedba39d8a25e4d6995b7838ce5aef4540
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-19 23:55:48 -0300

    feat: Implementa EnumExtension.cs em WebApp/Extensions.

---
+++ eAgenda.WebApp/Extensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace eAgenda.WebApp.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName<TEnum>(this TEnum enumValue) where TEnum : Enum
+    {
+        MemberInfo? memberInfo = typeof(TEnum).GetMember(enumValue.ToString()).FirstOrDefault();
+        DisplayAttribute? atributoDisplay = memberInfo?.GetCustomAttribute<DisplayAttribute>();
+
+        return atributoDisplay?.Name ?? enumValue.ToString();
+    }
+}
