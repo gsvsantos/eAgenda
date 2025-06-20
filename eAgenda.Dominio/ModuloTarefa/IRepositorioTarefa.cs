0f8a02f8e46fef468eb4837af4acff0a13a12da0
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-19 23:54:27 -0300

    feat: Implementa IRepositorioTarefa.cs em Dominio/ModuloTarefa.

---
+++ eAgenda.Dominio/ModuloTarefa/IRepositorioTarefa.cs
@@ -0,0 +1,19 @@
+﻿using eAgenda.Dominio.Compartilhado;
+
+namespace eAgenda.Dominio.ModuloTarefa;
+
+public interface IRepositorioTarefa : IRepositorio<Tarefa>
+{
+    public void AtualizarStatusRegistros();
+    public void ConcluirItensTarefa(Tarefa tarefa);
+    public void ReabrirItensTarefa(Tarefa tarefa);
+    public void CancelarItensTarefa(Tarefa tarefa);
+    public List<Tarefa> SelecionarTarefasPendentes();
+    public List<Tarefa> SelecionarTarefasEmAndamento();
+    public List<Tarefa> SelecionarTarefasConcluidas();
+    public List<Tarefa> SelecionarTarefasCanceladas();
+    public List<Tarefa> SelecionarTarefasPrioridadeBaixa();
+    public List<Tarefa> SelecionarTarefasPrioridadeMedia();
+    public List<Tarefa> SelecionarTarefasPrioridadeAlta();
+    public ItemTarefa SelecionarItem(Tarefa tarefa, Guid idItem);
+}
