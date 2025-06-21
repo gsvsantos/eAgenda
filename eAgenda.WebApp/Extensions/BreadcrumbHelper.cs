700ebae2b4f8962e335b708931a4344de9258e16
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-21 17:24:12 -0300

    feat: Implementa versão inicial do breadcrumb nas páginas.

---
+++ eAgenda.WebApp/Extensions/BreadcrumbHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace eAgenda.WebApp.Extensions;
+
+public static class BreadcrumbHelper
+{
+    public static IHtmlContent BuildBreadcrumbNavigation(this IHtmlHelper helper)
+    {
+        var viewContext = helper.ViewContext;
+        var routeValues = viewContext.RouteData.Values;
+
+        string controller = routeValues["controller"]?.ToString() ?? "";
+        string action = routeValues["action"]?.ToString() ?? "";
+
+        var urlHelperFactory = (IUrlHelperFactory)viewContext.HttpContext.RequestServices.GetService(typeof(IUrlHelperFactory))!;
+        var urlHelper = urlHelperFactory.GetUrlHelper(viewContext);
+
+        string breadcrumb = $"""
+            <a href="{urlHelper.Action("Index", "Home")}">Home</a> &gt;
+            <a href="{urlHelper.Action("Index", controller)}">{controller}</a> &gt;
+            {action}
+        """;
+
+        return new HtmlString(breadcrumb);
+    }
+}
