c4a2f45f9486ab8f2f4fe3e0965ec245ea40b3a4
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-23 03:41:08 -0300

    feat: Implementa estrutura inicial da página inicial.

--- eAgenda.WebApp/Controllers/HomeController.cs
+++ eAgenda.WebApp/Controllers/HomeController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eAgenda.Dominio.ModuloCategoria;
+using eAgenda.Dominio.ModuloDespesa;
+using eAgenda.Dominio.ModuloTarefa;
+using eAgenda.Infraestrutura.Arquivos.Compartilhado;
+using eAgenda.Infraestrutura.Arquivos.ModuloCategoria;
+using eAgenda.Infraestrutura.Arquivos.ModuloDespesa;
+using eAgenda.Infraestrutura.Arquivos.ModuloTarefa;
+using eAgenda.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eAgenda.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ContextoDados contextoDados;
+        private readonly IRepositorioTarefa repositorioTarefa;
+        private readonly IRepositorioDespesa repositorioDespesa;
+        //private readonly IRepositorioContato repositorioContato;
+        //private readonly IRepositorioCompromisso repositorioCompromisso;
+        private readonly IRepositorioCategoria repositorioCategoria;
+
+        public HomeController()
+        {
+            contextoDados = new(true);
+            repositorioCategoria = new RepositorioCatergoriaEmArquivo(contextoDados);
+            repositorioDespesa = new RepositorioDespesaEmArquivo(contextoDados);
+            repositorioTarefa = new RepositorioTarefaEmArquivos(contextoDados);
+        }
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel homeVM = new()
+            {
+                TotalCategorias = repositorioCategoria.SelecionarRegistros().Count,
+                TotalDespesas = repositorioDespesa.SelecionarRegistros().Sum(d => d.Valor),
+                UltimasDespesas = [.. repositorioDespesa.SelecionarRegistros()
+                                .OrderByDescending(d => d.DataOcorrencia)
+                                .Take(5)
+                                .Select(d => $"{d.Titulo} - R$ {d.Valor}")],
+                TotalTarefas = repositorioTarefa.SelecionarRegistros().Count,
+                UltimasTarefas = [.. repositorioTarefa.SelecionarRegistros()
+                                .OrderByDescending(t => t.DataCriacao)
+                                .Take(5)
+                                .Select(t => t.Titulo)],
+                TotalCompromissos = 42,
+                TotalContatos = 42,
+                ProximosCompromissos = ["teste", "teste"]
+            };
+
+            return View(homeVM);
         }
     }
 }
---
+++ eAgenda.WebApp/Models/HomeViewModels.cs
@@ -0,0 +1,25 @@
+﻿namespace eAgenda.WebApp.Models;
+
+public class HomeViewModel
+{
+    public int TotalTarefas { get; set; }
+    public int TotalContatos { get; set; }
+    public decimal TotalDespesas { get; set; }
+    public int TotalCompromissos { get; set; }
+    public int TotalCategorias { get; set; }
+    public List<string> UltimasTarefas { get; set; } = [];
+    public List<string> ProximosCompromissos { get; set; } = [];
+    public List<string> UltimasDespesas { get; set; } = [];
+
+    //public HomeViewModel(int totalTarefas, int totalContatos, decimal totalDespesas, int totalCompromissos, int totalCategorias, List<string> ultimasTarefas, List<string> proximosCompromissos, List<string> ultimasDespesas)
+    //{
+    //    TotalTarefas = totalTarefas;
+    //    TotalContatos = totalContatos;
+    //    TotalDespesas = totalDespesas;
+    //    TotalCompromissos = totalCompromissos;
+    //    TotalCategorias = totalCategorias;
+    //    UltimasTarefas = ultimasTarefas;
+    //    ProximosCompromissos = proximosCompromissos;
+    //    UltimasDespesas = ultimasDespesas;
+    //}
+}
