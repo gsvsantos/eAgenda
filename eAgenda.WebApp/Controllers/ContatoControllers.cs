8926dca5548069fdef240e83b900b250b58451ea
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-06-23 13:34:53 -0300

    feat: implementado modulo Contato no WebApp.

---
+++ eAgenda.WebApp/Controllers/ContatoControllers.cs
@@ -0,0 +1,128 @@
+﻿using eAgenda.Dominio.ModuloContato;
+using eAgenda.Infraestrutura.Arquivos.Compartilhado;
+using eAgenda.Infraestrutura.Arquivos.ModuloContato;
+using eAgenda.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAgenda.WebApp.Controllers
+{
+    [Route("contatos")]
+    public class ContatoController : Controller
+    {
+        private readonly ContextoDados contextoDados;
+        private readonly IRepositorioContato repositorioContato;
+
+        public ContatoController()
+        {
+            contextoDados = new ContextoDados(true);
+            repositorioContato = new RepositorioContatoEmArquivo(contextoDados);
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            List<Contato> contatos = repositorioContato.SelecionarRegistros();
+            VisualizarContatosViewModel visualizarVM = new VisualizarContatosViewModel(contatos);
+
+            return View(visualizarVM);
+        }
+
+        [HttpGet("cadastrar")]
+        public IActionResult Cadastrar()
+        {
+            
+            CadastrarContatoViewModel cadastrarVM = new CadastrarContatoViewModel();
+            return View(cadastrarVM);
+        }
+
+        [HttpPost("cadastrar")]
+        public IActionResult Cadastrar(CadastrarContatoViewModel cadastrarVM)
+        {
+            Contato contato = new Contato(cadastrarVM.Nome, cadastrarVM.Email, cadastrarVM.Telefone, cadastrarVM.Cargo!, cadastrarVM.Empresa!);
+
+            repositorioContato.CadastrarRegistro(contato);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("editar/{id:Guid}")]
+        public IActionResult Editar(Guid id)
+        {
+            Contato contato = repositorioContato.SelecionarRegistroPorId(id);
+            if (contato == null)
+                return NotFound();
+            EditarContatoViewModel editarVM = new EditarContatoViewModel(
+                contato.Id,
+                contato.Nome,
+                contato.Telefone,
+                contato.Email,
+                contato.Cargo,
+                contato.Empresa
+            );
+            return View(editarVM);
+        }
+
+        [HttpPost("editar/{id:Guid}")]
+        public IActionResult Editar(Guid id, EditarContatoViewModel editarVM)
+        {
+            //string? validacao = contato.Validar();
+            //if (!string.IsNullOrEmpty(validacao))
+            //{
+            //    ModelState.AddModelError("", validacao);
+            //    return View(contato);
+            //}
+
+            //if (repositorioContato.ExistePorEmailOuTelefone(contato.Email, contato.Telefone, id))
+            //{
+            //    ModelState.AddModelError("", "Já existe um contato com este e-mail ou telefone");
+            //    return View(contato);
+            //}
+            Contato contatoEditado = new Contato(
+                editarVM.Nome,
+                editarVM.Email,
+                editarVM.Telefone,
+                editarVM.Cargo!,
+                editarVM.Empresa!
+            );
+            repositorioContato.EditarRegistro(id,contatoEditado);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("excluir/{id:Guid}")]
+        public IActionResult Excluir(Guid id)
+        {
+            Contato contato = repositorioContato.SelecionarRegistroPorId(id);
+            if (contato == null)
+                return NotFound();
+
+            ExcluirContatoViewModel excluirVM = new ExcluirContatoViewModel(id, contato.Nome);
+
+            return View(excluirVM);
+        }
+
+        [HttpPost("excluir/{id:Guid}")]
+        public IActionResult ExcluirConfirmado(Guid id)
+        {
+            repositorioContato.ExcluirRegistro(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("detalhes/{id:Guid}")]
+        public IActionResult Detalhes(Guid id)
+        {
+            Contato contato = repositorioContato.SelecionarRegistroPorId(id);
+            if (contato == null)
+                return NotFound();
+            DetalhesContatoViewModel detalhesVM = new DetalhesContatoViewModel(
+                contato.Id,
+                contato.Nome,
+                contato.Email,
+                contato.Telefone,
+                contato.Cargo,
+                contato.Empresa,
+                contato.Compromissos);
+            return View(detalhesVM);
+        }
+    }
+
+}
---
+++ eAgenda.WebApp/Models/ContatoViewModel.cs
@@ -0,0 +1,141 @@
+﻿using eAgenda.Dominio.ModuloCompromisso;
+using eAgenda.Dominio.ModuloContato;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace eAgenda.WebApp.Models;
+
+public abstract class FormularioContatoViewModel
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? Cargo { get; set; }
+    public string? Empresa { get; set; }
+}
+public class CadastrarContatoViewModel : FormularioContatoViewModel
+{
+    public CadastrarContatoViewModel()
+    {
+
+    }
+    public CadastrarContatoViewModel(Guid id, string nome, string telefone, string email, string cargo, string empresa) : this()
+    {
+        Id = id;
+        Nome = nome;
+        Telefone = telefone;
+        Email = email;
+        Cargo = cargo;
+        Empresa = empresa;
+    }
+}
+public class VisualizarContatosViewModel
+{
+
+    public List<DetalhesContatoViewModel> Registros { get; set; } = [];
+
+    public VisualizarContatosViewModel(List<Contato> contatos)
+    {
+        foreach (Contato contato in contatos)
+        {
+            Registros.Add(new DetalhesContatoViewModel(
+                contato.Id,
+                contato.Nome,
+                contato.Email,
+                contato.Telefone,
+                contato.Cargo,
+                contato.Empresa,
+                contato.Compromissos));
+        }
+    }
+}
+public class EditarContatoViewModel : FormularioContatoViewModel
+{
+    public EditarContatoViewModel()
+    {
+
+    }
+    public EditarContatoViewModel(Guid id, string nome, string telefone, string email, string cargo, string empresa) : this()
+    {
+        Id = id;
+        Nome = nome;
+        Telefone = telefone;
+        Email = email;
+        Cargo = cargo;
+        Empresa = empresa;
+    }
+}
+
+public class ExcluirContatoViewModel : FormularioContatoViewModel
+{
+    public ExcluirContatoViewModel()
+    {
+
+    }
+    public ExcluirContatoViewModel(Guid id, string nome)    
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+public class DetalhesContatoViewModel
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    public string Cargo { get; set; }
+    public string Empresa { get; set; }
+    public List<CompromissoContatoViewModel> Compromissos { get; set; } = [];
+
+    public DetalhesContatoViewModel(Guid id, string nome, string email, string telefone, string cargo, string empresa, List<Compromisso> compromissos)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+        Cargo = cargo;
+        Empresa = empresa;
+
+        foreach (Compromisso compromisso in compromissos)
+        {
+            Compromissos.Add(new CompromissoContatoViewModel
+            (
+                compromisso.Assunto,
+                compromisso.DataOcorrencia,
+                compromisso.HoraInicio,
+                compromisso.HoraTermino,
+                compromisso.TipoCompromisso,
+                compromisso.Local,
+                compromisso.Link
+            ));
+        }
+    }
+}
+public class CompromissoContatoViewModel
+{
+    public CompromissoContatoViewModel(string assunto, DateTime dataOcorrencia, DateTime horaInicio, DateTime horaTermino, TipoCompromisso tipoCompromisso, string local, string link)
+    {
+        Assunto = assunto;
+        DataOcorrencia = dataOcorrencia;
+        HoraInicio = horaInicio;
+        HoraTermino = horaTermino;
+        TipoCompromisso = tipoCompromisso;
+        Local = local;
+        Link = link;
+    }
+
+    public string Assunto { get; set; } = string.Empty;
+    public DateTime DataOcorrencia { get; set; }
+
+    public DateTime HoraInicio { get; set; }
+
+    public DateTime HoraTermino { get; set; }
+
+    public TipoCompromisso TipoCompromisso { get; set; }
+
+    public string Local { get; set; } = string.Empty;
+
+    public string Link { get; set; } = string.Empty;
+
+}
