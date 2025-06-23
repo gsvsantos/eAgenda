807c83cd51a264bccb87811e81b1f1a22fe7ddc3
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-23 03:37:25 -0300

    refactor: Aprimora relacionamento dos Breadcrumb com as páginas.

--- eAgenda.WebApp/Extensions/BreadcrumbHelper.cs
+++ eAgenda.WebApp/Extensions/BreadcrumbHelper.cs
@@ -9,6 +9,13 @@ public static class BreadcrumbHelper
 {
     public static IHtmlContent BuildBreadcrumbNavigation(this IHtmlHelper helper)
     {
+        var controllerDisplayNames = new Dictionary<string, string>
+        {
+            { "Tarefa", "Tarefas" },
+            { "Categoria", "Categorias" },
+            { "Despesa", "Despesas" }
+        };
+
         var viewContext = helper.ViewContext;
         var routeValues = viewContext.RouteData.Values;
 
@@ -18,11 +25,28 @@ public static class BreadcrumbHelper
         var urlHelperFactory = (IUrlHelperFactory)viewContext.HttpContext.RequestServices.GetService(typeof(IUrlHelperFactory))!;
         var urlHelper = urlHelperFactory.GetUrlHelper(viewContext);
 
+        string controllerDisplay = controllerDisplayNames.ContainsKey(controller)
+             ? controllerDisplayNames[controller] : controller;
+
         string breadcrumb = $"""
-            <a href="{urlHelper.Action("Index", "Home")}">Home</a> &gt;
-            <a href="{urlHelper.Action("Index", controller)}">{controller}</a> &gt;
-            {action}
+            <a href="{urlHelper.Action("Index", "Home")}">Home</a>
         """;
+
+        if (!string.Equals(controller, "Home", StringComparison.OrdinalIgnoreCase))
+        {
+            breadcrumb += $"""
+                <span class="separator"><i class="bi bi-chevron-right fw-bold"></i></span>
+                <a href="{urlHelper.Action("Index", controller)}">{controllerDisplay}</a>
+            """;
+        }
+
+        if (!string.Equals(action, "Index", StringComparison.OrdinalIgnoreCase))
+        {
+            breadcrumb += $"""
+                <span class="separator"><i class="bi bi-chevron-right fw-bold"></i></span>
+                {action}
+            """;
+        }
 
         return new HtmlString(breadcrumb);
     }
