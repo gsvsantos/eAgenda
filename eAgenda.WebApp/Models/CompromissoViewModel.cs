2d9f0705bfa122bfd6a90596d023b62da3e223aa
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-23 23:05:06 -0300

    refactor: Implementa validações via TagHelpers e estilização no ModuloCompromisso.

--- eAgenda.WebApp/Models/CompromissoViewModel.cs
+++ eAgenda.WebApp/Models/CompromissoViewModel.cs
@@ -1,27 +1,55 @@
-﻿using eAgenda.Dominio.ModuloCompromisso;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using eAgenda.Dominio.ModuloCompromisso;
 using eAgenda.Dominio.ModuloContato;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-
 namespace eAgenda.WebApp.Models;
 
-public class CadastrarCompromissoViewModel
+public abstract class FormularioCompromissoViewModel
 {
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Digite o Assunto.")]
+    [DisplayName("Assunto")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "O assunto do compromisso deve ter entre 2 e 100 caracteres.")]
     public string Assunto { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Selecione a data do compromisso.")]
+    [DisplayName("Data do Compromisso")]
+    [DataType(DataType.DateTime)]
     public DateTime DataOcorrencia { get; set; }
+
+    [Required(ErrorMessage = "Insira o horário de início.")]
+    [DisplayName("Hora de Início")]
+    [DataType(DataType.DateTime)]
     public DateTime HoraInicio { get; set; }
+
+    [Required(ErrorMessage = "Insira o horário de término.")]
+    [DisplayName("Hora de Término")]
+    [DataType(DataType.DateTime)]
     public DateTime HoraTermino { get; set; }
+
+    [Required(ErrorMessage = "Selecione uma prioridade.")]
+    [DisplayName("Prioridade")]
     public TipoCompromisso TipoCompromisso { get; set; }
+
+    [DisplayName("Local")]
+    [StringLength(100, ErrorMessage = "O local deve ter no máximo 100 caracteres.")]
     public string? Local { get; set; } = string.Empty;
+
+    [DisplayName("Link")]
+    [StringLength(200, ErrorMessage = "O link deve ter no máximo 200 caracteres.")]
     public string? Link { get; set; } = string.Empty;
+
+    [DisplayName("Contato")]
     public Guid ContatoId { get; set; } = Guid.Empty;
-
     public List<SelectListItem> Contatos { get; set; } = [];
+}
 
-    public CadastrarCompromissoViewModel()
-    {
-
-    }
+public class CadastrarCompromissoViewModel : FormularioCompromissoViewModel
+{
+    public CadastrarCompromissoViewModel() { }
 
     public CadastrarCompromissoViewModel(List<Contato> contatos)
     {
@@ -36,10 +64,8 @@ public class CadastrarCompromissoViewModel
     }
 }
 
-public class EditarCompromissoViewModel : CadastrarCompromissoViewModel
+public class EditarCompromissoViewModel : FormularioCompromissoViewModel
 {
-    public Guid Id { get; set; }
-
     public EditarCompromissoViewModel() { }
 
     public EditarCompromissoViewModel(Compromisso compromisso, List<Contato> contatos)
@@ -63,11 +89,9 @@ public class EditarCompromissoViewModel : CadastrarCompromissoViewModel
         }
     }
 }
-public class ExcluirCompromissoViewModel
+public class ExcluirCompromissoViewModel : FormularioCompromissoViewModel
 {
-    public Guid Id { get; set; }
-    public string Assunto { get; set; }
-
+    public ExcluirCompromissoViewModel() { }
     public ExcluirCompromissoViewModel(Guid id, string assunto)
     {
         Id = id;
@@ -124,8 +148,3 @@ public class DetalhesCompromissoViewModel
         Contato = contato;
     }
 }
-
-
-
-
-
