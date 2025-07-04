bb8944cf4cf1d73e293fb2f11915cfab48389075
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-04 18:57:38 -0300

    feat: Implementa novas querys de criação das tabelas.

--- tabelas.sql
+++ tabelas.sql
@@ -1,5 +1,15 @@
+CREATE TABLE [dbo].[TBContato] (
+    [Id]       UNIQUEIDENTIFIER NOT NULL,
+    [Nome]     NVARCHAR (100)   NOT NULL,
+    [Telefone] NVARCHAR (20)    NOT NULL,
+    [Email]    NVARCHAR (50)    NOT NULL,
+    [Empresa]  NVARCHAR (100)   NULL,
+    [Cargo]    NVARCHAR (100)   NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
 CREATE TABLE [dbo].[TBCompromisso] (
-    [Id]          UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
     [Assunto]     NVARCHAR (100)   NOT NULL,
     [Data]        DATETIME2 (7)    NOT NULL,
     [HoraInicio]  BIGINT           NOT NULL,
@@ -12,30 +22,43 @@ CREATE TABLE [dbo].[TBCompromisso] (
     CONSTRAINT [FK_TBCompromisso_TBContato] FOREIGN KEY ([Contato_Id]) REFERENCES [dbo].[TBContato] ([Id])
 );
 
-CREATE TABLE [dbo].[TBContato] (
-    [Id]       UNIQUEIDENTIFIER NOT NULL,
-    [Nome]     NVARCHAR (100)   NOT NULL,
-    [Telefone] NVARCHAR (20)    NOT NULL,
-    [Email]    NVARCHAR (50)    NOT NULL,
-    [Empresa]  NVARCHAR (100)   NULL,
-    [Cargo]    NVARCHAR (100)   NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
 CREATE TABLE [dbo].[TBTarefa] (
     [Id]            UNIQUEIDENTIFIER NOT NULL,
-    [Titulo]        NVARCHAR(100) NOT NULL,
-    [DataCriacao]   DATETIME2 NOT NULL,
-    [DataConclusao] DATETIME2 NULL,
-    [Concluida]     BIT NOT NULL
+    [Titulo]        NVARCHAR (100)   NOT NULL,
+    [Descricao]     NVARCHAR (100)   NOT NULL,
+    [Prioridade]    INT              NOT NULL,
+    [DataCriacao]   DATETIME2 (7)    NOT NULL,
+    [DataConclusao] DATETIME2 (7)    NULL,
+    [Status]        INT              NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
 CREATE TABLE [dbo].[TBItemTarefa] (
-    [Id]       UNIQUEIDENTIFIER NOT NULL,
-    [Titulo]   NVARCHAR(100) NOT NULL,
-    [Concluido] BIT NOT NULL,
+    [Id]        UNIQUEIDENTIFIER NOT NULL,
+    [Titulo]    NVARCHAR (100)   NOT NULL,
+    [Status]    INT              NOT NULL,
     [Tarefa_Id] UNIQUEIDENTIFIER NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_TBItemTarefa_TBTarefa] FOREIGN KEY ([Tarefa_Id]) REFERENCES [dbo].[TBTarefa] ([Id])
+);
+
+CREATE TABLE [dbo].[TBDespesa] (
+    [Id]             UNIQUEIDENTIFIER NOT NULL,
+    [Titulo]         NVARCHAR (100)   NOT NULL,
+    [Descricao]      NVARCHAR (100)   NOT NULL,
+    [DataOcorrencia] DATETIME2 (7)    NOT NULL,
+    [Valor]          DECIMAL (18, 2)  NOT NULL,
+    [FormaPagamento] INT              NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[TBCategoria] (
+    [Id]     UNIQUEIDENTIFIER NOT NULL,
+    [Titulo] NVARCHAR (100)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[TBDespesa_TBCategoria] (
+    [Despesa_Id]   UNIQUEIDENTIFIER NOT NULL,
+    [Categoria_Id] UNIQUEIDENTIFIER NOT NULL
 );
