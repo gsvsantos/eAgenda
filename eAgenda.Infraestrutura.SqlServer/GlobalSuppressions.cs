1aece07fccca50acc9806a6ae1de4ef0b582b9b8
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-08 20:25:48 -0300

    feat: Implementa supressão de warning IDE0290.

---
+++ eAgenda.Infraestrutura.SqlServer/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:eAgenda.Infraestrutura.SQLServer.ModuloCategoria.RepositorioCategoriaSQL.#ctor(System.Data.IDbConnection)")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:eAgenda.Infraestrutura.SQLServer.ModuloCompromisso.RepositorioCompromissoSQL.#ctor(System.Data.IDbConnection)")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:eAgenda.Infraestrutura.SQLServer.ModuloContato.RepositorioContatoSQL.#ctor(System.Data.IDbConnection)")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:eAgenda.Infraestrutura.SQLServer.ModuloDespesa.RepositorioDespesaSQL.#ctor(System.Data.IDbConnection)")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:eAgenda.Infraestrutura.SQLServer.ModuloTarefa.RepositorioTarefaSQL.#ctor(System.Data.IDbConnection)")]
