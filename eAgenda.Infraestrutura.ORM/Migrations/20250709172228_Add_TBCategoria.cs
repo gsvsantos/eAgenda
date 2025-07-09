30e3403e332d704d81dd9f90fa24a1a324e2bfb1
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-09 14:35:07 -0300

    feat: Implementada configuração do DBCategoria.

---
+++ eAgenda.Infraestrutura.ORM/Migrations/20250709172228_Add_TBCategoria.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eAgenda.Infraestrutura.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_TBCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Despesa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataOcorrencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FormaPagamento = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Despesa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoriaDespesa",
+                columns: table => new
+                {
+                    CategoriasId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DespesasId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriaDespesa", x => new { x.CategoriasId, x.DespesasId });
+                    table.ForeignKey(
+                        name: "FK_CategoriaDespesa_Categorias_CategoriasId",
+                        column: x => x.CategoriasId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoriaDespesa_Despesa_DespesasId",
+                        column: x => x.DespesasId,
+                        principalTable: "Despesa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoriaDespesa_DespesasId",
+                table: "CategoriaDespesa",
+                column: "DespesasId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoriaDespesa");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Despesa");
+        }
+    }
+}
---
+++ eAgenda.Infraestrutura.ORM/ModuloCategoria/MapeadorCategoriaORM.cs
@@ -0,0 +1,22 @@
+﻿using eAgenda.Dominio.ModuloCategoria;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAgenda.Infraestrutura.ORM.ModuloCategoria;
+
+public class MapeadorCategoriaORM : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.Property(c => c.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Titulo)
+            .IsRequired();
+
+        builder.Property(c => c.Despesas);
+    }
+}
