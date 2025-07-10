8af0efeacc34b8a241f192f53bc47e75e29b9e8d
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-10 14:59:09 -0300

    feat: Implementa ModuloCompromisso com ORM.

---
+++ eAgenda.Infraestrutura.ORM/ModuloCompromisso/RepositorioCompromissoORM.cs
@@ -0,0 +1,21 @@
+﻿using eAgenda.Dominio.ModuloCompromisso;
+using eAgenda.Infraestrutura.ORM.Compartilhado;
+
+namespace eAgenda.Infraestrutura.ORM.ModuloCompromisso;
+
+public class RepositorioCompromissoORM : RepositorioBaseORM<Compromisso>, IRepositorioCompromisso
+{
+    public RepositorioCompromissoORM(EAgendaDbContext contexto) : base(contexto)
+    {
+    }
+
+    public List<Compromisso> SelecionarCompromissosContato(Guid idRegistro)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TemConflito(Compromisso compromisso)
+    {
+        throw new NotImplementedException();
+    }
+}
