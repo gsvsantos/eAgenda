004f95fd6378dc5c4a896e562fb2b55ad58f1805
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-10 14:59:00 -0300

    feat: Implementa ModuloContato com ORM.

---
+++ eAgenda.Infraestrutura.ORM/ModuloContato/MapeadorContatoORM.cs
@@ -0,0 +1,41 @@
+﻿using eAgenda.Dominio.ModuloContato;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAgenda.Infraestrutura.ORM.ModuloConta;
+
+public class MapeadorContatoORM : IEntityTypeConfiguration<Contato>
+{
+    public void Configure(EntityTypeBuilder<Contato> builder)
+    {
+        builder.ToTable("TBContato");
+
+        builder.Property(c => c.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(c => c.Nome)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        builder.Property(c => c.Email)
+            .HasMaxLength(254)
+            .IsRequired();
+
+        builder.Property(c => c.Telefone)
+            .HasMaxLength(30)
+            .IsRequired();
+
+        builder.Property(c => c.Cargo)
+            .HasMaxLength(150)
+            .IsRequired(false);
+
+        builder.Property(c => c.Empresa)
+            .HasMaxLength(200)
+            .IsRequired(false);
+
+        builder.HasMany(c => c.Compromissos)
+            .WithOne(com => com.Contato)
+            .IsRequired(false);
+    }
+}
---
+++ eAgenda.Infraestrutura.ORM/ModuloContato/RepositorioContatoORM.cs
@@ -0,0 +1,21 @@
+﻿using eAgenda.Dominio.ModuloContato;
+using eAgenda.Infraestrutura.ORM.Compartilhado;
+
+namespace eAgenda.Infraestrutura.ORM.ModuloConta;
+
+public class RepositorioContatoORM : RepositorioBaseORM<Contato>, IRepositorioContato
+{
+    public RepositorioContatoORM(EAgendaDbContext contexto) : base(contexto)
+    {
+    }
+
+    public bool ExistePorEmailOuTelefone(string email, string telefone, Guid? ignorarId = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool PossuiCompromissosVinculados(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
