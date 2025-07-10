d47ee9fc7f131007ee19a57279e66c93862986c6
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-09 23:49:44 -0300

    feat: Implementa OnModelCreating() em eAgendaDbContext para melhor funcionamento nas criações de tabelas.

--- eAgenda.Infraestrutura.ORM/Compartilhado/eAgendaDbContext.cs
+++ eAgenda.Infraestrutura.ORM/Compartilhado/eAgendaDbContext.cs
@@ -1,4 +1,7 @@
 ﻿using eAgenda.Dominio.ModuloCategoria;
+using eAgenda.Dominio.ModuloDespesa;
+using eAgenda.Infraestrutura.ORM.ModuloCategoria;
+using eAgenda.Infraestrutura.ORM.ModuloDespesa;
 using Microsoft.EntityFrameworkCore;
 
 namespace eAgenda.Infraestrutura.ORM.Compartilhado;
@@ -7,4 +10,11 @@ public class eAgendaDbContext : DbContext
 {
     public eAgendaDbContext(DbContextOptions options) : base(options) { }
     public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Despesa> Despesas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new MapeadorCategoriaORM());
+        modelBuilder.ApplyConfiguration(new MapeadorDespesaORM());
+    }
 }
