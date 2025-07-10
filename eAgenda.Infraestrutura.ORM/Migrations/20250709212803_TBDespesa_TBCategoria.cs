e38f83783b25ab519f20d4995240a6da9aefa0b3
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-09 23:51:32 -0300

    feat: Implementa migrations das tabelas TBCategoria, TBDespesa e relação entre elas.

---
+++ eAgenda.Infraestrutura.ORM/Migrations/20250709212803_TBDespesa_TBCategoria.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eAgenda.Infraestrutura.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class TBDespesa_TBCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBCategoria",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBCategoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBDespesa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataOcorrencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    FormaPagamento = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBDespesa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBDespesa_TBCategoria",
+                columns: table => new
+                {
+                    CategoriasId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DespesasId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBDespesa_TBCategoria", x => new { x.CategoriasId, x.DespesasId });
+                    table.ForeignKey(
+                        name: "FK_TBDespesa_TBCategoria_TBCategoria_CategoriasId",
+                        column: x => x.CategoriasId,
+                        principalTable: "TBCategoria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TBDespesa_TBCategoria_TBDespesa_DespesasId",
+                        column: x => x.DespesasId,
+                        principalTable: "TBDespesa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBDespesa_TBCategoria_DespesasId",
+                table: "TBDespesa_TBCategoria",
+                column: "DespesasId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBDespesa_TBCategoria");
+
+            migrationBuilder.DropTable(
+                name: "TBCategoria");
+
+            migrationBuilder.DropTable(
+                name: "TBDespesa");
+        }
+    }
+}
