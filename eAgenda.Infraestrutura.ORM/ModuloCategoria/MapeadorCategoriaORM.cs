ff130ee47fd2a48db485c4154773dba9d33fa010
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-09 23:48:21 -0300

    feat: Implementa ModuloCategoria com ORM com correções nas configurações de DBCategoria.

--- eAgenda.Infraestrutura.ORM/ModuloCategoria/MapeadorCategoriaORM.cs
+++ eAgenda.Infraestrutura.ORM/ModuloCategoria/MapeadorCategoriaORM.cs
@@ -8,15 +8,16 @@ public class MapeadorCategoriaORM : IEntityTypeConfiguration<Categoria>
 {
     public void Configure(EntityTypeBuilder<Categoria> builder)
     {
+        builder.ToTable("TBCategoria");
+
         builder.Property(c => c.Id)
             .ValueGeneratedNever()
             .IsRequired();
 
-        builder.HasKey(c => c.Id);
-
         builder.Property(c => c.Titulo)
             .IsRequired();
 
-        builder.Property(c => c.Despesas);
+        builder.HasMany(c => c.Despesas)
+            .WithMany(d => d.Categorias);
     }
 }
---
+++ eAgenda.Infraestrutura.ORM/ModuloCategoria/RepositorioCategoriaORM.cs
@@ -0,0 +1,56 @@
+﻿using eAgenda.Dominio.ModuloCategoria;
+using eAgenda.Infraestrutura.ORM.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAgenda.Infraestrutura.ORM.ModuloCategoria;
+
+public class RepositorioCategoriaORM : IRepositorioCategoria
+{
+    private readonly eAgendaDbContext contexto;
+
+    public RepositorioCategoriaORM(eAgendaDbContext contexto)
+    {
+        this.contexto = contexto;
+    }
+
+    public void CadastrarRegistro(Categoria novoRegistro)
+    {
+        novoRegistro.Id = Guid.NewGuid();
+        contexto.Categorias.Add(novoRegistro);
+    }
+
+    public bool EditarRegistro(Guid idRegistro, Categoria registroEditado)
+    {
+        Categoria? categoriaSelecionada = SelecionarRegistroPorId(idRegistro);
+
+        if (categoriaSelecionada is null)
+            return false;
+
+        categoriaSelecionada.AtualizarRegistro(registroEditado);
+
+        return true;
+    }
+
+    public bool ExcluirRegistro(Guid idRegistro)
+    {
+        Categoria? categoriaSelecionada = SelecionarRegistroPorId(idRegistro);
+
+        if (categoriaSelecionada is null)
+            return false;
+
+        contexto.Categorias.Remove(categoriaSelecionada);
+
+        return true;
+    }
+
+    public Categoria? SelecionarRegistroPorId(Guid idRegistro)
+    {
+
+        return contexto.Categorias.Where(c => c.Id.Equals(idRegistro)).Include(c => c.Despesas).FirstOrDefault();
+    }
+
+    public List<Categoria> SelecionarRegistros()
+    {
+        return [.. contexto.Categorias.Include(c => c.Despesas)];
+    }
+}
